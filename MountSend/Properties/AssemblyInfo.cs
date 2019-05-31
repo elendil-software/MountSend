a4a7790d9b967c9787f8d703e2b37678634aff94
Julien Tschäppät <git@elendil.software>
2019-05-31 16:22:10 +0200

    AssemblyInfo corrections

--- MountSend/Properties/AssemblyInfo.cs
+++ MountSend/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MountSend")]
-[assembly: AssemblyDescription("10Micron Mount Fixer by Per Frejvall")]
+[assembly: AssemblyDescription("10Micron Mount Sender by Per Frejvall, Julien Tschäppät")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MountSend")]
-[assembly: AssemblyCopyright("Copyright © 2012-2016 Per Frejvall")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016 Per Frejvall, 2016-2019 Julien Tschäppät")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyFileVersion("3.3.0")]
